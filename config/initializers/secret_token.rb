3af4d4b9d2757585422d3fed2828b4d801d30f71
heartsentwined <heartsentwined.me@gmail.com>
2013-09-24 08:19:04 +0800

    test case

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EpfTest::Application.config.secret_key_base = '105fb823dbd5bad65fe6184535c23fb30940739342d6ad604e0e7ab2ba42655b912c97c1c6d867153beeba4a2d6547ca26d84b5109dfb6c25b9e4c1528dbc55c'
